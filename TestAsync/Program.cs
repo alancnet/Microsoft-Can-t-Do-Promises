d695c0a2097a3d9ba5fc508080eef456918e9197
Alan Colon <alco@uei.com>
2017-07-11 14:52:15 -0700

    Code

--- TestAsync/Program.cs
+++ TestAsync/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace TestAsync
 {
@@ -6,7 +7,21 @@ namespace TestAsync
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var r = SayHello();
+            r.ContinueWith((str) => {
+                Console.WriteLine("This prints: {0}", str.Result);
+                r.ContinueWith((str2) => {
+                    Console.WriteLine("This does not print: {0}", str2.Result);
+				});
+            });
+        }
+        static Task<string> SayHi() {
+            var ret = new TaskCompletionSource<string>();
+            ret.SetResult("Hello World");
+            return ret.Task;
+        }
+        static async Task<string> SayHello() {
+            return await SayHi();
         }
     }
 }
